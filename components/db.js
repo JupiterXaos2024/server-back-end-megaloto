bebf867985289ea72c646e1baa543d17fee8d1e2
JupiterXaos2024 <149838940+JupiterXaos2024@users.noreply.github.com>
2024-07-11 20:17:46 -0500

    agregamos database

--- components/db.js
+++ components/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Configura la conexión a la base de datos
 const pool = mysql.createPool({
-  host: 'luzdeinnovacion.com', // El host de tu base de datos
-  user: 't304329_Jupiter', // El usuario de tu base de datos
-  password: '9]#nv4*dl#;X~ek35N', // La contraseña de tu base de datos
-  database: 't304329_Loto' // El nombre de tu base de datos
+  host:'193.84.177.252',
+  database:'t304329_Loto',
+  user:'t304329_Jupiter',
+  password:'9]#nv4*dl#;X~ek35N'
 });
 
 module.exports = pool.promise();
